e9f642d2ff3ff8aa719dd0bcc6d19d37ec17d928
Négyesi Zoltán <lblrazor@gmail.com>
2021-12-04 12:07:36 +0100

    ExitTest

--- src/test/java/hu/nye/progtech/torpedo/service/interactions/impl/ExitTest.java
+++ src/test/java/hu/nye/progtech/torpedo/service/interactions/impl/ExitTest.java
@@ -1,8 +1,11 @@
 package hu.nye.progtech.torpedo.service.interactions.impl;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import hu.nye.progtech.torpedo.model.GameState;
+import hu.nye.progtech.torpedo.service.game.StepController;
 import hu.nye.progtech.torpedo.ui.TablePrinter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,12 +24,14 @@ public class ExitTest {
     private Exit underTest;
     private GameState gameState;
     private TablePrinter tablePrinter;
+    private StepController stepController;
 
     @BeforeEach
     public void setUp() {
         gameState = new GameState(null, null);
         tablePrinter = Mockito.mock(TablePrinter.class);
         underTest = new Exit(tablePrinter, gameState);
+        stepController = Mockito.mock(StepController.class);
     }
 
     @Test
@@ -38,6 +43,51 @@ public class ExitTest {
 
         // then
         assertTrue(result);
+    }
+
+    @Test
+    public void testIsEqualToCommandShouldReturnFalseWhenInputIsNotExit() {
+        // given in setup
+
+        // when
+        boolean result = underTest.isEqualToCommand(NOT_EXIT_COMMAND);
+
+        // then
+        assertFalse(result);
+    }
+
+    @Test
+    public void testProcessShouldChangeShouldExitFieldOfGameState() {
+        // given in setup
+
+        // when
+        underTest.process(EXIT_COMMAND, stepController);
+
+        // then
+        assertTrue(gameState.isShouldExit());
+    }
+
+    @Test
+    public void testSetUsableShouldSetUsableFieldOfExit() {
+        // given in setup
+
+
+        // when
+        underTest.setUsable(false);
+
+        // then
+        assertFalse(underTest.isUsable());
+    }
+
+    @Test
+    public void testGetNameShouldReturnExitCommandFieldOfExit() {
+        // given in setup
+
+        //when
+        String result = underTest.getName();
+
+        // then
+        assertEquals(result, EXIT_COMMAND);
     }
 
 
